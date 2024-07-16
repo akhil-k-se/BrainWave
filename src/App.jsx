2ac55a4a7ed44189d000ca85350ca3285191cf05
akhil-k-se <akhil.se2024@gmail.com>
2024-07-16 09:32:23 +0530

    final Code

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Header from '../src/components/Header'
+import Hero from './components/Hero';
+import Benefits from './components/Benefits';
+import Colloration from './components/Colloration';
+import Services from './components/Services';
+import Pricing from './components/Pricing';
+import Roadmap from './components/Roadmap';
+import Footer from './components/Footer';
+
+const App = () => {
+  return(
+    <>
+      
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header/>
+        <Hero/>
+        <Benefits/>
+        <Colloration/>
+        <Services/>
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
+      </div>
+      <ButtonGradient/>
+    </>
+  );
+};
+
+export default App;
